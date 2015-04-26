aea09a0488e381ade21522de02280d0776e15b8f
anabranch <wac.chambers@gmail.com>
2015-04-26 14:07:33 -0700

    Now using Flask Restful

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python
 from flask import Flask, jsonify, make_response
+from flask.ext import restful
+
 from sqlalchemy import Column, Integer, String, Date, \
     Time, Float, create_engine, func
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
 from geoalchemy2 import Geometry, WKTElement
 from collections import defaultdict
-
+import psycopg2
 
 app = Flask(__name__)
+api = restful.Api(app)
 
 ## database
-Base = declarative_base()
+database = 'postgresql://info247:test@localhost:5432/sfpd'
+engine = create_engine(database)
+Session = sessionmaker(bind=engine)
+session = Session()
 
+crime_api_v1 = "/api/v1/crime/"
+
+def response_decorator(func):
+    """Provides a decorator function to
+    embed our response within a response object"""
+    def func_wrap(*args, **kwargs):
+        return {"response": func(*args, **kwargs)}
+    return func_wrap
+
+Base = declarative_base()
 class Crime(Base):
+    """
+    Crime Table
+    """
     __tablename__ = 'crime'
     incidntnum = Column(Integer)
     category = Column(String)
@@ -29,43 +49,25 @@ class Crime(Base):
     pdid = Column(Integer, primary_key=True)
     geom = Column(Geometry('geography'))
 
-database = 'postgresql://info247:test@localhost:5432/sfpd'
-engine = create_engine(database)
-Session = sessionmaker(bind=engine)
-session = Session()
 
 
-## views
-@app.route('/api/v1/polygon/<coordinates>', methods=['GET'])
-def index(coordinates):
-    '''
-    Converts URL Encoded (Space delimited) points
-    to a polygon and retrieves all crime within that area
+class CrimePolygon(restful.Resource):
+    def get(self, coordinates):
+        try:
+            poly_query = WKTElement('POLYGON((' + coordinates + '))', srid=4326)
+            crime_cat = session.query(Crime.category, func.count(Crime.category)) \
+                .filter(Crime.geom.ST_Intersects(poly_query)) \
+                .group_by(Crime.category)
 
-    Example:
-    # View all crimes in Dolores Park by crime category
-
-    $ curl -i 'http://localhost:5000/api/v1/polygon/%2D122.42841124534607%2037.76128348360843%2C%2D122.42810010910034%2037.7580942260561%2C%2D122.42584705352783%2037.75822145970878%2C%2D122.42613673210143%2037.76141071177564%2C%2D122.42841124534607%2037.76128348360843'
-    '''
-
-    poly_query = WKTElement('POLYGON((' + coordinates + '))', srid=4326)
-
-    crime_cat = session.query(Crime.category, func.count(Crime.category)) \
-        .filter(Crime.geom.ST_Intersects(poly_query)) \
-        .group_by(Crime.category)
-
-    crime_dict = defaultdict(list)
-    for crime in crime_cat:
-        crime_dict[crime[0]] = crime[1]
-# @TODO Need to add error handling for malformed requests
-    return jsonify({'crimes': crime_dict})
+            crime_dict = defaultdict(list)
+            for crime in crime_cat:
+                crime_dict[crime[0]] = crime[1]
+            return crime_dict, 200
+        except:
+            return {}, 400
 
 
-@app.errorhandler(404)
-def not_found(error):
-    """Simple 404 Error Handling"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
+api.add_resource(CrimePolygon, crime_api_v1 + "polygon/<coordinates>")
 
 if __name__ == '__main__':
     app.run(debug=True)
